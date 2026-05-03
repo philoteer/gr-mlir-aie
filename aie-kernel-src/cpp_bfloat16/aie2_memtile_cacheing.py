396e0ef925abc5b00abe1050719314720de198eb
ksshin <ksshin.mail@gmail.com>
2026-05-04 04:06:13 +0900

    add memtile example

--- aie-kernel-src/cpp_bfloat16/aie2_memtile_cacheing.py
+++ aie-kernel-src/cpp_bfloat16/aie2_memtile_cacheing.py
@@ -5,7 +5,11 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 #
 # (c) Copyright 2025 Advanced Micro Devices, Inc. or its affiliates
+
+#example for memtile cacheing.
+
 import numpy as np
+import ml_dtypes
 import sys
 
 from aie.iron import Kernel, ObjectFifo, Program, Runtime, Worker
@@ -16,12 +20,12 @@ import aie.iron as iron
 
 dev = iron.get_current_device()
 
-tensor_size = 4096
+tensor_size = 8192
 tile_size = tensor_size // 4
 
 # Define tensor types
-tensor_ty = np.ndarray[(tensor_size,), np.dtype[np.int32]]
-tile_ty = np.ndarray[(tile_size,), np.dtype[np.int32]]
+tensor_ty = np.ndarray[(tensor_size,), np.dtype[ml_dtypes.bfloat16]]
+tile_ty = np.ndarray[(tile_size,), np.dtype[ml_dtypes.bfloat16]]
 scalar_ty = np.ndarray[(1,), np.dtype[np.int32]]
 
 # External, binary kernel definition
@@ -33,6 +37,7 @@ scale_fn = Kernel(
 
 # Input data movement
 of_in = ObjectFifo(tile_ty, name="in")
+of_in_mem = of_in.cons().forward(name="in_mem")
 
 # Output data movement
 of_out = ObjectFifo(tile_ty, name="out")
@@ -43,13 +48,13 @@ def core_fn(of_in, of_out, scale_scalar):
     for _ in range_(4):
         elem_in = of_in.acquire(1)
         elem_out = of_out.acquire(1)
-        scale_scalar(elem_in, elem_out, 1024)
+        scale_scalar(elem_in, elem_out, 2048)
         of_in.release(1)
         of_out.release(1)
 
 
 # Create a worker to perform the task
-my_worker = Worker(core_fn, [of_in.cons(), of_out.prod(), scale_fn])
+my_worker = Worker(core_fn, [of_in_mem.cons(), of_out.prod(), scale_fn])
 
 # Runtime operations to move data to/from the AIE-array
 rt = Runtime()
