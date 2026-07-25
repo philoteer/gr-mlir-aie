fe3de35c4b85e5f0f840bec901986b0b642a02ab
ksshin <ksshin.mail@gmail.com>
2026-07-25 16:08:05 +0900

    .

--- gr-mlir_aie/python/mlir_aie/bindings/python_bindings.cc
+++ gr-mlir_aie/python/mlir_aie/bindings/python_bindings.cc
@@ -38,6 +38,7 @@ namespace py = pybind11;
     void bind_cint16_to_complex64(py::module& m);
     void bind_Q15_to_float32(py::module& m);
     void bind_cint32_to_complex64(py::module& m);
+    void bind_int32_Q_to_float32(py::module& m);
 // ) END BINDING_FUNCTION_PROTOTYPES
 
 
@@ -83,5 +84,6 @@ PYBIND11_MODULE(mlir_aie_python, m)
     bind_cint16_to_complex64(m);
     bind_Q15_to_float32(m);
     bind_cint32_to_complex64(m);
+    bind_int32_Q_to_float32(m);
     // ) END BINDING_FUNCTION_CALLS
 }
