e4d495de59e82b7d716d26d9726e271539acfe62
ksshin <ksshin.mail@gmail.com>
2026-08-11 23:05:37 +0900

    .

--- gr-mlir_aie/python/mlir_aie/bindings/python_bindings.cc
+++ gr-mlir_aie/python/mlir_aie/bindings/python_bindings.cc
@@ -46,6 +46,7 @@ namespace py = pybind11;
     void bind_mlir_aie_cpp_tagged_int32_to_int64(py::module& m);
     void bind_mlir_aie_cpp_fft_test(py::module& m);
     void bind_mlir_aie_80211_phy(py::module& m);
+    void bind_mlir_aie_cpp_equalizer_test(py::module& m);
 // ) END BINDING_FUNCTION_PROTOTYPES
 
 
@@ -99,5 +100,6 @@ PYBIND11_MODULE(mlir_aie_python, m)
     bind_mlir_aie_cpp_tagged_int32_to_int64(m);
     bind_mlir_aie_cpp_fft_test(m);
     bind_mlir_aie_80211_phy(m);
+    bind_mlir_aie_cpp_equalizer_test(m);
     // ) END BINDING_FUNCTION_CALLS
 }
